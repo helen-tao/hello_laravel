d628480a4ab83026abbb40de128d87faec7ae7fd
helen <yun.helen.tao@gmail.com>
2022-10-12 18:43:21 +1000

    saba 1.0

--- app/Http/Controllers/SabaController.php
+++ app/Http/Controllers/SabaController.php
@@ -3,21 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class SabaController extends Controller
 {
     function make ()
     {
-        $info = 'my sunshine';
-        echo $info;
-        $infoes=config('saba_info');
-        foreach ($infoes as $key => $value) {
+        $info_array=config('saba_info');
+        foreach ($info_array as $key => $value) {
             $info = array();
-            $info['file_name'] = $key;
-            //dd($value);
+            $info['file_name'] = $key.".html";
+            //dd(storage_path());
             $content=view('saba.default',$value);
-            //$info['content'] = view('saba.default',$value)->render(): string;
-            echo $content;
+            if(Storage::put("test/".$info['file_name'], $content)){
+                echo $info['file_name']." file is suc<p>";
+            }else{
+                echo $info['file_name']." file is fail<p>";
+            }
+            //echo $content;
         };
     }
 }
